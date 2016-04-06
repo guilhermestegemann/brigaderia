2ca8dc5acbdc95356d6f7b6cd1c55741240aca3a
guilhermestegemann <guilherme.stegemann@gmail.com>
2016-04-05 22:12:49 -0300

    0504 aula

--- WebContent/resources/faturamento/pedidoCompra/js/pedidoCompra.js
+++ WebContent/resources/faturamento/pedidoCompra/js/pedidoCompra.js
@@ -2,9 +2,38 @@ BRIGADERIA.pedidoCompra = new Object();
 
 $(document).ready( function () {
 	
-	
 	var produtos;
 	var produtoArray = []; //utilizado ao inserir ingrediente no formulário
+	
+	$("#subConteudo").text(""); //inicia div vazia
+	
+	$("#produtos").on('change',function(){
+		for (var i = 0; i < produtos.length; i++) {
+			if (produtos[i].codigoProduto == $("#produtos").val()) {
+				$("#unEntrada").val(produtos[i].unEntrada);
+			}
+		}
+		
+	});
+	
+	$("#qtdeProduto").on('blur', function(){
+		if ($("#unitario").val() != "") {
+			$("#totalItemPedidoCompra").val($("#unitario").val().replace(",",".") * ($("#qtdeProduto").val().replace(",",".")));
+		}
+		if ($("#totalItemPedidoCompra").val() != "") {
+			$("#totalItemPedidoCompra").val(parseFloat($("#totalItemPedidoCompra").val()).toFixed(2));
+		}
+	});
+	
+	$("#unitario").on('blur', function(){
+		if ($("#unitario").val() != "") {
+			$("#totalItemPedidoCompra").val(parseFloat($("#unitario").val().replace(",",".") * $("#qtdeProduto").val().replace(",",".")).toFixed(2));
+		}
+		if ($("#unitario").val().replace(",",".") > 0) {
+			$("#unitario").val(parseFloat($("#unitario").val().replace(",",".")).toFixed(2));
+		}
+	});
+
 	
 	BRIGADERIA.pedidoCompra.listarProdutos = function () { 
 		BRIGADERIA.pedidoCompraService.listarProdutos({
@@ -13,7 +42,7 @@ $(document).ready( function () {
 				produtos = data;
 
 				for (var i = 0; i < data.length; i++) {
-					html += "<option value='" + data[i].codigo + "'>" + data[i].descricao + "</option>";
+					html += "<option value='" + data[i].codigoProduto + "'>" + data[i].descricao + "</option>";
 				}
 				$("#produtos").append(html);
 			},
@@ -25,6 +54,92 @@ $(document).ready( function () {
 	
 	BRIGADERIA.pedidoCompra.exibirFormulario = function () {
 		BRIGADERIA.pedidoCompra.listarProdutos();
+		$("#totalPedidoCompra").val("0.00");
 	};
 	
+	BRIGADERIA.pedidoCompra.incluirProduto = function () {
+		var expNumeros = /^[0-9]+$/;
+		if ($("#produtos").val() == "") {
+			bootbox.alert("Selecione o Produto!");
+		}else if (!expNumeros.test($("#qtdeProduto").val().replace(",","").replace(".",""))) {
+			bootbox.alert("Quantida inválida.");
+		}else if (($("#unitario").val() == "") || ($("#unitario").val() <= 0)) {
+			bootbox.alert("Unitário inválido.");
+		}else{
+			var html = "";
+			var descricao = "";
+			var unEntrada = "";
+			
+			for (var i = 0; i < produtos.length; i++) {
+		
+				if ((produtos[i].codigoProduto == $("#produtos").val()) && (unEntrada == "")) {
+					unEntrada = produtos[i].unEntrada;
+					descricao = produtos[i].descricao; 
+				}
+			}
+			
+			if (unEntrada != "") {
+				html =  "<tr class='itemPedidoCompra'>"
+						  + "<td >" + $("#produtos").val() + "</td>"
+						  + "<td>" + descricao + "</td>"
+						  + "<td>" + unEntrada + "</td>"
+						  + "<td>" + $("#qtdeProduto").val().replace(",",".") + "</td>"
+						  + "<td>" + parseFloat($("#unitario").val().replace(",",".")).toFixed(2) + "</td>"
+						  + "<td>" + $("#totalItemPedidoCompra").val() + "</td>"
+						  + "<td><a href='#'><i class='glyphicon glyphicon-remove-sign' onclick='BRIGADERIA.pedidoCompra.deletarProduto(this"+ "," + $("#produtos").val() + "," + "\"" + descricao + "\"" + "," + parseFloat($("#qtdeProduto").val().replace(",",".")) + "," + "\"" + unEntrada + "\"" + "," + parseFloat($("#unitario").val()) + "," + parseFloat($("#totalItemPedidoCompra").val()) +")' aria-hidden='true'></i></a>"
+					  + "</tr>";
+				$("#totalPedidoCompra").val(parseFloat(parseFloat($("#totalItemPedidoCompra").val()) + parseFloat($("#totalPedidoCompra").val())).toFixed(2)) // soma o total do pedido.
+				var prod = {
+						codigo: $("#produtos").val(),
+						qtde: $("#qtdeProduto").val(),
+						unitario: $("#unitario").val(),
+						total: $("#totalItemPedidoCompra").val()
+				} ;
+				
+				produtoArray.push(prod);
+				$("#itensPedidoCompra tbody").append(html);
+				$("#produtos option:selected").remove();
+				$("#qtdeProduto").val("");
+				$("#unitario").val("");
+				$("#totalItemPedidoCompra").val("");
+				$("#unEntrada").val("");
+			}
+		}
+	};
+	
+	BRIGADERIA.pedidoCompra.deletarProduto = function (handler, codigo, descricao, qtde, unEntrada, unitario, totalItem) {
+		$("#qtdeProduto").val(parseFloat(qtde));
+		$("#unEntrada").val(unEntrada);
+		$("#unitario").val(parseFloat(unitario));
+		$("#totalItemPedidoCompra").val(totalItem);
+		for (var i = 0; i < produtoArray.length; i++) {
+			if (produtoArray[i].codigo == codigo) {
+				produtoArray.splice(i, 1);
+			}
+		}
+		var tr = $(handler).closest('tr');
+		tr.remove();  
+		window.event.preventDefault();
+		$('#produtos').append('<option value="' + codigo + '" selected="selected">' + descricao + '</option>');
+		$("#totalPedidoCompra").val(parseFloat(parseFloat($("#totalPedidoCompra").val()) - parseFloat(totalItem)).toFixed(2)) //diminui do total do pedido.
+	};
+	
+	BRIGADERIA.pedidoCompra.adicionar = function() {
+		var newPedido = {
+			total : $("#totalPedidoCompra").val(),
+			produtos: produtoArray
+		};
+		
+		retornoValida = BRIGADERIA.validaProdutos.validar(newData.produto);
+		if (retornoValida != "") {
+			bootbox.alert("Favor verificar os seguintes campos: " + retornoValida);
+		}else{
+			retornoValida = BRIGADERIA.validaFichaTecnica.validar(newData.fichaTecnica);
+			if (retornoValida == "") {
+				BRIGADERIA.produtoFichaTecnicaService.adicionar(newData);
+			}else{
+				bootbox.alert("Favor verificar os seguintes campos: " + retornoValida);
+			}
+		}
+	};
 });
--- WebContent/resources/gerenciar/pedidoCompra/js/gerenciarPedidoCompra.js
+++ WebContent/resources/gerenciar/pedidoCompra/js/gerenciarPedidoCompra.js
@@ -2,6 +2,8 @@ BRIGADERIA.gerenciarPedidoCompra = new Object();
 
 $(document).ready( function () {
 	
+	$("#subConteudo").text(""); //inicia div vazia
+	
 	BRIGADERIA.gerenciarPedidoCompra.lancarPedido = function() {
 		$("#conteudo").load("resources/faturamento/pedidoCompra/pedidoCompra.html", function (){
 		BRIGADERIA.pedidoCompra.exibirFormulario();
